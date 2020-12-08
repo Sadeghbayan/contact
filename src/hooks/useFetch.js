3179be4d087783acceb917ef7cf045d7000e6775
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-12-08 12:10:17 +0330

    fix missing depnedenci on useEffect

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {groupContactsBasedOnLetters} from '../utils/helper'
 
 const useFetch = (url, loading) => {
@@ -6,25 +6,25 @@ const useFetch = (url, loading) => {
 	const [response, setResponse] = useState(null);
 	const [isLoading, setIsLoading] = useState(loading);
 
-	const getDataFromAPI = async () => {
-		setIsLoading(true)
-		if (cache.current[url]) {
-			const data = cache.current[url];
-			const contacts = groupContactsBasedOnLetters(data)
-			setResponse(contacts);
-			setIsLoading(false)
-		} else {
-			const response = await fetch(url);
-			const data = await response.json();
-			cache.current[url] = data; // set response in cache;
-			const contacts = groupContactsBasedOnLetters(data)
-			setResponse(contacts);
-			setIsLoading(false)
-		}
-	}
-
 	useEffect(() => {
-		 getDataFromAPI()
+
+		const getDataFromAPI = async () => {
+			if (cache.current[url]) {
+				const data = cache.current[url];
+				const contacts = groupContactsBasedOnLetters(data)
+				setResponse(contacts);
+				setIsLoading(false)
+			} else {
+				const response = await fetch(url);
+				const data = await response.json();
+				cache.current[url] = data; // set response in cache;
+				const contacts = groupContactsBasedOnLetters(data)
+				setResponse(contacts);
+				setIsLoading(false)
+			}
+		}
+
+		getDataFromAPI()
 	}, [url]);
 
 	return { response, isLoading };
